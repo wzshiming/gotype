2ec2f39f4a6a0ae6063d6f027b76b3624b6375c2
wzshiming <wzshiming@foxmail.com>
2018-05-24 16:11:59 +0800

    update note

--- builtinfunc.go
+++ builtinfunc.go
@@ -3,12 +3,12 @@ package gotype
 //go:generate stringer -type builtinfunc builtinfunc.go
 type builtinfunc uint8
 
-// 内置函数 的参数计算
+// Built-in function results
 const (
 	_                    builtinfunc = iota
-	builtinfuncInt                   // 默认 int
-	builtinfuncPtrItem               // 第一个参数的指针
-	builtinfuncItem                  // 第一个参数
-	builtinfuncInterface             // 接口
-	builtinfuncVoid                  // 无
+	builtinfuncInt                   // Returns int
+	builtinfuncPtrItem               // Returns the type pointer of the first parameter
+	builtinfuncItem                  // Returns the first parameter
+	builtinfuncInterface             // Returns interface
+	builtinfuncVoid                  // Returns void
 )
