660059bd47492a4978e202c08e1962934fa45bf9
wzshiming <wzshiming@foxmail.com>
2019-01-04 15:54:44 +0800

    set iota is number

--- constant.go
+++ constant.go
@@ -14,18 +14,21 @@ func constantEval(expr ast.Node, iota int64, info *info) (r constant.Value, err 
 		switch t.Kind {
 		case token.STRING:
 			r = constant.MakeString(t.Value[1 : len(t.Value)-1])
+			return r, nil
 		case token.INT:
 			i, err := strconv.ParseInt(t.Value, 0, 0)
 			if err != nil {
 				return nil, err
 			}
 			r = constant.MakeInt64(i)
+			return r, nil
 		case token.FLOAT:
 			i, err := strconv.ParseFloat(t.Value, 0)
 			if err != nil {
 				return nil, err
 			}
 			r = constant.MakeFloat64(i)
+			return r, nil
 		}
 	case *ast.UnaryExpr:
 		x, err := constantEval(t.X, iota, info)
@@ -36,6 +39,7 @@ func constantEval(expr ast.Node, iota int64, info *info) (r constant.Value, err 
 		if err != nil {
 			return nil, err
 		}
+		return r, nil
 	case *ast.BinaryExpr:
 		x, err := constantEval(t.X, iota, info)
 		if err != nil {
@@ -49,9 +53,11 @@ func constantEval(expr ast.Node, iota int64, info *info) (r constant.Value, err 
 		if err != nil {
 			return nil, err
 		}
+		return r, nil
 	case *ast.Ident:
 		if t.Name == "iota" {
 			r = constant.MakeInt64(iota)
+			return r, nil
 		} else if val, ok := info.Named.Search(t.Name); ok && val.Kind() == Declaration {
 			val = val.Declaration()
 			switch val.Kind() {
@@ -61,23 +67,33 @@ func constantEval(expr ast.Node, iota int64, info *info) (r constant.Value, err 
 					return nil, err
 				}
 				r = constant.MakeInt64(i)
+				return r, nil
 			case Uint, Uint8, Uint16, Uint32, Uint64:
 				i, err := strconv.ParseUint(val.Value(), 0, 0)
 				if err != nil {
 					return nil, err
 				}
 				r = constant.MakeUint64(i)
+				return r, nil
 			case Float32, Float64:
 				i, err := strconv.ParseFloat(val.Value(), 0)
 				if err != nil {
 					return nil, err
 				}
 				r = constant.MakeFloat64(i)
+				return r, nil
 			case String:
 				str := val.Value()
 				r = constant.MakeString(str[1 : len(str)-1])
+				return r, nil
+			default:
+				// iota
+				i, err := strconv.ParseInt(val.Value(), 0, 0)
+				if err == nil {
+					r = constant.MakeInt64(i)
+					return r, nil
+				}
 			}
-
 		} else {
 			return nil, fmt.Errorf("undefined ident")
 		}
@@ -86,6 +102,7 @@ func constantEval(expr ast.Node, iota int64, info *info) (r constant.Value, err 
 		if err != nil {
 			return nil, err
 		}
+		return r, nil
 	case *ast.CallExpr:
 		if len(t.Args) == 0 {
 			return nil, fmt.Errorf("undefined call")
@@ -94,10 +111,11 @@ func constantEval(expr ast.Node, iota int64, info *info) (r constant.Value, err 
 		if err != nil {
 			return nil, err
 		}
+		return r, nil
 	default:
 		return nil, fmt.Errorf("undefined expr")
 	}
-	return r, nil
+	return constant.MakeUnknown(), nil
 }
 
 func constantUnaryOp(op token.Token, y constant.Value) (r constant.Value, err error) {
@@ -108,7 +126,7 @@ func constantUnaryOp(op token.Token, y constant.Value) (r constant.Value, err er
 	}()
 
 	r = constant.UnaryOp(op, y, 0)
-	return
+	return r, nil
 }
 
 func constantBinaryOp(op token.Token, x, y constant.Value) (r constant.Value, err error) {
@@ -126,5 +144,5 @@ func constantBinaryOp(op token.Token, x, y constant.Value) (r constant.Value, er
 	default:
 		r = constant.BinaryOp(x, op, y)
 	}
-	return
+	return r, nil
 }
