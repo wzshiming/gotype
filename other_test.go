baba3d5589f36ee31638df88b1b2a31fe95e039c
wzshiming <wzshiming@foxmail.com>
2019-01-04 16:10:32 +0800

    update test

--- other_test.go
+++ other_test.go
@@ -2,6 +2,7 @@ package gotype
 
 import (
 	"fmt"
+	"go/token"
 	"reflect"
 	"strconv"
 	"strings"
@@ -15,25 +16,34 @@ func TestOhter(t *testing.T) {
 		"github.com/wzshiming/gotype/testdata/struct",
 	}
 	for _, src := range testpath {
-		testAll(t, Import(t, src))
+		testAll(t, src)
 	}
 }
 
-func testAll(t *testing.T, scope Type) {
+func testAll(t *testing.T, src string) {
+	imp := getImporter(t)
+	scope, err := imp.Import(src)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fset := imp.FileSet()
 	num := scope.NumChild()
 	for i := 0; i != num; i++ {
 		v := scope.Child(i)
-		testType(t, v)
+		testType(t, fset, v)
 	}
 }
 
-func testType(t *testing.T, v Type) {
+func testType(t *testing.T, fset *token.FileSet, v Type) {
 
 	for _, line := range strings.Split(v.Doc().Text(), "\n") {
 		if line == "" {
 			continue
 		}
 		v := v
+
+		pos := fset.Position(v.Origin().Pos())
+
 		tag := reflect.StructTag(line)
 		if data, ok := tag.Lookup("To"); ok {
 			st := strings.Split(data, ",")
@@ -48,139 +58,139 @@ func testType(t *testing.T, v Type) {
 					v = v.Key()
 				case "In":
 					if len(method) < 2 {
-						t.Fatal("Error In num: ", to)
+						t.Fatal(pos, "Error In num: ", to)
 					}
 					i, _ := strconv.ParseInt(method[1], 10, 64)
 					v = v.In(int(i))
 				case "Out":
 					if len(method) < 2 {
-						t.Fatal("Error Out num: ", to)
+						t.Fatal(pos, "Error Out num: ", to)
 					}
 					i, _ := strconv.ParseInt(method[1], 10, 64)
 					v = v.Out(int(i))
 				case "Field":
 					if len(method) < 2 {
-						t.Fatal("Error Field num: ", to)
+						t.Fatal(pos, "Error Field num: ", to)
 					}
 					i, err := strconv.ParseInt(method[1], 10, 64)
 					if err != nil {
-						t.Fatal("Error Field: ", err)
+						t.Fatal(pos, "Error Field: ", err)
 					}
 					if v.NumField() <= int(i) {
-						t.Fatal("Error Out of index range: ", to)
+						t.Fatal(pos, "Error Out of index range: ", to)
 					}
 					v = v.Field(int(i))
 				case "Method":
 					if len(method) < 2 {
-						t.Fatal("Error Method num: ", to)
+						t.Fatal(pos, "Error Method num: ", to)
 					}
 					i, err := strconv.ParseInt(method[1], 10, 64)
 					if err != nil {
-						t.Fatal("Error Method: ", err)
+						t.Fatal(pos, "Error Method: ", err)
 					}
 					if v.NumMethod() <= int(i) {
-						t.Fatal("Error Out of index range: ", to)
+						t.Fatal(pos, "Error Out of index range: ", to)
 					}
 					v = v.Method(int(i))
 				case "Child":
 					if len(method) < 2 {
-						t.Fatal("Error Child num: ", to)
+						t.Fatal(pos, "Error Child num: ", to)
 					}
 					i, err := strconv.ParseInt(method[1], 10, 64)
 					if err != nil {
-						t.Fatal("Error Child: ", err)
+						t.Fatal(pos, "Error Child: ", err)
 					}
 					if v.NumMethod() <= int(i) {
-						t.Fatal("Error Out of index range: ", to)
+						t.Fatal(pos, "Error Out of index range: ", to)
 					}
 					v = v.Child(int(i))
 				case "FieldByName":
 					if len(method) < 2 {
-						t.Fatal("Error FieldByName num: ", to)
+						t.Fatal(pos, "Error FieldByName num: ", to)
 					}
 					v, ok = v.FieldByName(method[1])
 					if !ok {
-						t.Fatal("Error not found: ", to)
+						t.Fatal(pos, "Error not found: ", to)
 					}
 				case "MethodByName":
 					if len(method) < 2 {
-						t.Fatal("Error MethodByName num: ", to)
+						t.Fatal(pos, "Error MethodByName num: ", to)
 					}
 					v, ok = v.MethodByName(method[1])
 					if !ok {
-						t.Fatal("Error not found: ", to)
+						t.Fatal(pos, "Error not found: ", to)
 					}
 				case "ChildByName":
 					if len(method) < 2 {
-						t.Fatal("Error ChildByName num: ", to)
+						t.Fatal(pos, "Error ChildByName num: ", to)
 					}
 					v, ok = v.ChildByName(method[1])
 					if !ok {
-						t.Fatal("Error not found: ", to)
+						t.Fatal(pos, "Error not found: ", to)
 					}
 				default:
-					t.Fatal("Error to: ", to)
+					t.Fatal(pos, "Error to: ", to)
 				}
 			}
 		}
 		if data, ok := tag.Lookup("Value"); ok {
 			val := v.Value()
 			if data != val {
-				t.Fatal("Error value:", val, data)
+				t.Fatal(pos, "Error value:", val, data)
 			}
 		}
 		if data, ok := tag.Lookup("Name"); ok {
 			name := v.Name()
 			if data != name {
-				t.Fatal("Error name:", name, data)
+				t.Fatal(pos, "Error name:", name, data)
 			}
 		}
 		if data, ok := tag.Lookup("String"); ok {
 			str := v.String()
 			if data != str {
-				t.Fatal("Error string:", str, data)
+				t.Fatal(pos, "Error string:", str, data)
 			}
 		}
 		if data, ok := tag.Lookup("Kind"); ok {
 			kind := v.Kind().String()
 			if data != kind {
-				t.Fatal("Error kind:", kind, data)
+				t.Fatal(pos, "Error kind:", kind, data)
 			}
 		}
 		if data, ok := tag.Lookup("Len"); ok {
 			l := fmt.Sprint(v.Len())
 			if data != l {
-				t.Fatal("Error len:", l, data)
+				t.Fatal(pos, "Error len:", l, data)
 			}
 		}
 		if data, ok := tag.Lookup("NumMethod"); ok {
 			num := fmt.Sprint(v.NumMethod())
 			if data != num {
-				t.Fatal("Error num method:", num, data)
+				t.Fatal(pos, "Error num method:", num, data)
 			}
 		}
 		if data, ok := tag.Lookup("NumIn"); ok {
 			num := fmt.Sprint(v.NumIn())
 			if data != num {
-				t.Fatal("Error num in:", num, data)
+				t.Fatal(pos, "Error num in:", num, data)
 			}
 		}
 		if data, ok := tag.Lookup("NumOut"); ok {
 			num := fmt.Sprint(v.NumOut())
 			if data != num {
-				t.Fatal("Error num out:", num, data)
+				t.Fatal(pos, "Error num out:", num, data)
 			}
 		}
 		if data, ok := tag.Lookup("NumField"); ok {
 			num := fmt.Sprint(v.NumField())
 			if data != num {
-				t.Fatal("Error num field:", num, data)
+				t.Fatal(pos, "Error num field:", num, data)
 			}
 		}
 		if data, ok := tag.Lookup("Tag"); ok {
 			num := string(v.Tag())
 			if data != num {
-				t.Fatal("Error tag:", num, data)
+				t.Fatal(pos, "Error tag:", num, data)
 			}
 		}
 	}
--- utils_test.go
+++ utils_test.go
@@ -5,12 +5,8 @@ import (
 )
 
 func Parse(t *testing.T, src string) Type {
-	typ, err := NewImporter(
-		WithCommentLocator(),
-		ErrorHandler(func(err error) {
-			t.Error(err)
-		}),
-	).ImportSource("_", []byte(src))
+	imp := getImporter(t)
+	typ, err := imp.ImportSource("_", []byte(src))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,14 +14,21 @@ func Parse(t *testing.T, src string) Type {
 }
 
 func Import(t *testing.T, path string) Type {
-	typ, err := NewImporter(
-		WithCommentLocator(),
-		ErrorHandler(func(err error) {
-			t.Error(err)
-		}),
-	).Import(path)
+	imp := getImporter(t)
+	typ, err := imp.Import(path)
 	if err != nil {
 		t.Fatal(err)
 	}
 	return typ
+
+}
+
+func getImporter(t *testing.T) *Importer {
+	imp := NewImporter(
+		WithCommentLocator(),
+		ErrorHandler(func(err error) {
+			t.Error(err)
+		}),
+	)
+	return imp
 }
